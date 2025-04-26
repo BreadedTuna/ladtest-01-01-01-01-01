300237bad5e814ab5f5844aa599aca0e501951da
James Phelps <jamesph2012@outlook.com>
2025-04-26 18:29:28 +0100

    Update 404.js

--- 404.js
+++ 404.js
@@ -1,7 +1,13 @@
-document.body.innerHTML = `<html><head><title>404 Not Found</title></head>
+// Remove all styles and fonts
+document.querySelectorAll('style, link[rel="stylesheet"], link[rel="preload"][as="style"], link[rel="preload"][as="font"], style, font, *[style]').forEach(e => e.remove());
+
+// Reset document head and body
+document.head.innerHTML = '';
+document.body.innerHTML = `
+<html>
+<head><title>404 Not Found</title></head>
 <body>
 <center><h1>404 Not Found</h1></center>
 <hr><center>nginx/1.18.0 (Ubuntu)</center>
-
-
-</body></html>`
+</body>
+</html>`;
